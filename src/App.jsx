ad571f3219da54bd2547d41966f5a21e51ba3fb5
Devi <167757357+visakadev@users.noreply.github.com>
2025-06-15 14:32:05 +0200

    Remove commented-out Pizza component and related code from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import Pizza from "./Pizza";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
-// const Pizza = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props?.name),
-//     React.createElement("p", {}, props?.description),
-//   ]);
-// };
+
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Devi"),
-  //   React.createElement(Pizza, {
-  //     name: "Pizza1",
-  //     description: "Tomato",
-  //     key: 0,
-  //   }),
-  //   React.createElement(Pizza, {
-  //     name: "Pizza1",
-  //     description: "Tomato",
-  //     key: 1,
-  //   }),
-  // ]);
   return (
     <StrictMode>
       <div>
         <h1>Pizza 2025</h1>
-        {/* <Pizza name="Margarita"  description="Tomato Basil" />
-       <Pizza name="Marinara"  description="Tomato" /> */}
         <Order />
         <PizzaOfTheDay />
       </div>
