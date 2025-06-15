b7b143a318d2f337a00f2c7e71e1e61772f60aa2
Devi <167757357+visakadev@users.noreply.github.com>
2025-06-15 22:21:40 +0200

    feat: add ErrorBoundary component to handle errors in the application

---
+++ src/ErrorBoundry.jsx
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+  };
+  static getDerivedStateFromError() {
+    return {
+      hasError: true,
+    };
+  }
+  componentDidCatch(error, info) {
+    // sentry
+    console.error("ErrorBoundary:", error);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-boundary">
+          <h2>Uh oh!</h2>
+          <p>
+            There was an error with this page
+            <Link to="/">Click Here</Link> to go back to the home Page.
+          </p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
