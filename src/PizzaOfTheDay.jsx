90e819491f99a6d1ce07e7c13573b29d712d20b1
Devi <167757357+visakadev@users.noreply.github.com>
2025-06-16 17:25:12 +0200

    Add new pizza images and style updates
    - Added multiple new pizza images in .webp format to the public/pizzas directory.
    - Created a new CSS file for styling the application with custom properties for colors and fonts.
    - Updated the PizzaOfTheDay component to enhance the layout and improve accessibility by wrapping price in a span.

--- src/PizzaOfTheDay.jsx
+++ src/PizzaOfTheDay.jsx
@@ -11,18 +11,20 @@ export default function pizzaOfTheDay() {
   }
   return (
     <div className="pizza-of-the-day">
-      <h2>Pizza of the day</h2>
-      <div className="pizza-of-the-day-info">
-        <h3>{pizzaOfTheDay.name}</h3>
-        <p>{pizzaOfTheDay.description}</p>
-        <p className="pizza-of-the-day-price">
-          From: {intl.format(pizzaOfTheDay.sizes.S)}
-        </p>
+      <h2>Pizza of the Day</h2>
+      <div>
+        <div className="pizza-of-the-day-info">
+          <h3>{pizzaOfTheDay.name}</h3>
+          <p>{pizzaOfTheDay.description}</p>
+          <p className="pizza-of-the-day-price">
+            From: <span>{intl.format(pizzaOfTheDay.sizes.S)}</span>
+          </p>
+        </div>
         <img
           className="pizza-of-the-day-image"
           src={pizzaOfTheDay.image}
           alt={pizzaOfTheDay.name}
-        ></img>
+        />
       </div>
     </div>
   );
