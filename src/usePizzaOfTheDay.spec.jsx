b69a9896eb4cd41f7b1aa647e25334efc75bbd22
Devi <167757357+visakadev@users.noreply.github.com>
2025-06-16 15:05:50 +0200

    feat: add vitest-fetch-mock for testing and implement tests for contact form and pizza of the day

---
+++ src/usePizzaOfTheDay.spec.jsx
@@ -0,0 +1,34 @@
+import { test, vi, expect } from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
+import createFetchMock from "vitest-fetch-mock";
+import { usePizzaOfTheDay } from "./usePizzaOfTheDay";
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+const testPizza = {
+  id: "spinach_fet",
+  name: "The Spinach and Feta Pizza",
+  category: "Veggie",
+  description: "Spinach, Mushrooms, Red Onions, Feta Cheese, Garlic",
+  image: "/public/pizzas/spinach_fet.webp",
+  sizes: {
+    S: 12,
+    M: 16,
+    L: 20.25,
+  },
+};
+test("gives null when first called", async () => {
+  fetch.mockResponseOnce(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay());
+  expect(result.current).toBeNull();
+});
+
+test("to call the API and give back the pizza of the day", async () => {
+  fetch.mockResponseOnce(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay(""));
+  await waitFor(() => {
+    expect(result.current).toEqual(testPizza);
+  });
+  expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
+});
